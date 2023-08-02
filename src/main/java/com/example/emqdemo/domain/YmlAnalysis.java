b49b1c6a93c41deb2ad1c9f43b60b6efbaa3336e
wangkaihui <wangkkhh@126.com>
2023-08-02 10:41:03 +0800

    yml文件

---
+++ src/main/java/com/example/emqdemo/domain/YmlAnalysis.java
@@ -0,0 +1,17 @@
+package com.example.emqdemo.domain;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "custom.map")
+public class YmlAnalysis {
+    Map<String,Object> value;
+}
+
+
+
